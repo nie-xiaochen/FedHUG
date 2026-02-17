b9fd54421007c97afe0aaf1f90cdda3dbc08167d
nie-xiaochen <18831064657@163.com>
2026-02-17 21:26:52 +0800

    Add files via upload

---
+++ gen_utils/generate_utils.py
@@ -0,0 +1,113 @@
+import torch
+from torch.utils.data import Dataset
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Generator(nn.Module):
+    def __init__(self, nz=100, ngf=64, img_size=32, nc=3):
+        super(Generator, self).__init__()
+
+        self.init_size = img_size // 4
+        self.l1 = nn.Sequential(nn.Linear(nz, ngf * 2 * self.init_size ** 2))
+
+        self.conv_blocks = nn.Sequential(
+            nn.BatchNorm2d(ngf * 2),
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(ngf * 2, ngf * 2, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(ngf * 2, ngf, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ngf, nc, 3, stride=1, padding=1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, z):
+        out = self.l1(z)
+        out = out.view(out.shape[0], -1, self.init_size, self.init_size)
+        img = self.conv_blocks(out)
+        return img
+    
+
+        
+class DeepInversionHook():
+ 
+
+    def __init__(self, module):
+        self.hook = module.register_forward_hook(self.hook_fn)
+        self.module = module
+
+    def hook_fn(self, module, input, output):  # hook_fn(module, input, output) -> None
+        # hook co compute deepinversion's feature distribution regularization
+        nch = input[0].shape[1]
+        mean = input[0].mean([0, 2, 3])
+        var = input[0].permute(1, 0, 2, 3).contiguous().view([nch, -1]).var(1, unbiased=False)
+        # forcing mean and variance to match between two distributions
+        # other ways might work better, i.g. KL divergence
+        r_feature = torch.norm(module.running_var.data - var, 2) + torch.norm(
+            module.running_mean.data - mean, 2)
+        self.r_feature = r_feature
+
+    def remove(self):
+        self.hook.remove()
+
+class GenImageDataset(Dataset):
+    def __init__(self, data, output, transform=None):
+        self.data = data
+        self.output = output
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        img = self.data[idx]
+        if self.transform:
+            img = self.transform(img)
+        label = self.output[idx]
+        return img, label
+
+    def __len__(self):
+        return self.data.shape[0]
+    
+class MultiTransform:
+
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, x):
+        return [t(x) for t in self.transform]
+
+    def __repr__(self):
+        return str(self.transform)
+    
+def reset_model(model):
+    for m in model.modules():
+        if isinstance(m, (nn.ConvTranspose2d, nn.Linear, nn.Conv2d)):
+            nn.init.normal_(m.weight, 0.0, 0.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        if isinstance(m, (nn.BatchNorm2d)):
+            nn.init.normal_(m.weight, 1.0, 0.02)
+            nn.init.constant_(m.bias, 0)
+
+
+def kldiv(logits, targets, T=1.0, reduction='batchmean'):
+    q = F.log_softmax(logits / T, dim=1)
+    p = F.softmax(targets / T, dim=1)
+    return F.kl_div(q, p, reduction=reduction) * (T * T)
+
+class KLDiv(nn.Module):
+    def __init__(self, T=1.0, reduction='batchmean'):
+        """
+
+        :rtype: object
+        """
+        super().__init__()
+        self.T = T
+        self.reduction = reduction
+
+    def forward(self, logits, targets):
+        return kldiv(logits, targets, T=self.T, reduction=self.reduction)
+
